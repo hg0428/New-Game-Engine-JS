4959b628d8499314cf1ee707583a8ba02cc8774a
hg0428 <45984899+hg0428@users.noreply.github.com>
2022-06-02 13:41:58 +0000

    Major speed improvements

--- script.js
+++ script.js
@@ -204,43 +204,43 @@ class Game {
         return this._data.x;
       } set x(val) {
         this._data.x = val;
-        this._data.left = val - this.width / 2;
-        this._data.right = val + this.width / 2;
+        this._data.left = val - this._data.width / 2;
+        this._data.right = val + this._data.width / 2;
       }
       get y() {
         return this._data.y;
       } set y(val) {
         this._data.y = val;
-        this._data.top = val - this.height / 2;
-        this._data.bottom = val + this.height / 2;
+        this._data.top = val - this._data.height / 2;
+        this._data.bottom = val + this._data.height / 2;
       }
       get left() {
         return this._data.left;
       } set left(val) {
         this._data.left = val;
-        this._data.x = val + this.width / 2;
-        this._data.right = val + this.width;
+        this._data.x = val + this._data.width / 2;
+        this._data.right = val + this._data.width;
       }
       get top() {
         return this._data.top;
       } set top(val) {
         this._data.top = val;
-        this._data.y = val + this.height / 2;
-        this._data.bottom = val + this.height;
+        this._data.y = val + this._data.height / 2;
+        this._data.bottom = val + this._data.height;
       }
       get right() {
         return this._data.right;
       } set right(val) {
         this._data.right = val;
-        this._data.x = val - this.width / 2;
-        this._data.left = val - this.width;
+        this._data.x = val - this._data.width / 2;
+        this._data.left = val - this._data.width;
       }
       get bottom() {
         return this._data.bottom;
       } set bottom(val) {
         this._data.bottom = val;
-        this._data.y = val - this.height / 2;
-        this._data.top = val - this.height;
+        this._data.y = val - this._data.height / 2;
+        this._data.top = val - this._data.height;
       }
       get width() {
         return this._data.width;
@@ -283,14 +283,14 @@ class Game {
 
       getCollider() {
         /*let rectCollider = {
-            left: this.left,
-            top: this.top,
-            right: this.right,
-            bottom: this.bottom,
-            width: this.width,
-            height: this.height,
-            x: this.x,
-            y: this.y,
+            left: this._data.left,
+            top: this._data.top,
+            right: this._data.right,
+            bottom: this._data.bottom,
+            width: this._data.width,
+            height: this._data.height,
+            x: this._data.x,
+            y: this._data.y,
             prevX: this.prevX,
             prevY: this.prevY
         }
@@ -329,8 +329,8 @@ class Game {
       }
 
       posUpdate() {
-        this.realX = this.left + (parent.width / 2);
-        this.realY = this.top + (parent.height / 2);
+        this.realX = this._data.left + (parent.width / 2);
+        this.realY = this._data.top + (parent.height / 2);
         if (!this.overhead) {
           this.realX += parent.camera.offsetX;
           this.realY += parent.camera.offsetY;
@@ -377,23 +377,23 @@ class Game {
 
           //We use 1/2 width and height a lot, maybe we should just add that as an attribute.
           //That would save us from recalculating it every time.
-          let t1HalfW = this.width / 2;
-          let t1HalfH = this.height / 2;
-          let t2HalfW = other.width / 2;
-          let t2HalfH = other.height / 2;
+          let t1HalfW = this._data.width / 2;
+          let t1HalfH = this._data.height / 2;
+          let t2HalfW = other._data.width / 2;
+          let t2HalfH = other._data.height / 2;
 
           // Only check objects that are in range
           if (
             this.distanceTo(other) < rectCollider2.width +
-            rectCollider.width +
-            rectCollider2.height +
-            rectCollider.height &&
+            rectCollider._data.width +
+            rectCollider2._data.height +
+            rectCollider._data.height &&
             rectCollide(rectCollider, rectCollider2)
           ) {
 
             // Calculate the distance between centers
-            let diffX = this.x - other.x;
-            let diffY = this.y - other.y;
+            let diffX = this._data.x - other._data.x;
+            let diffY = this._data.y - other._data.y;
 
             // Calculate the minimum distance to separate along X and Y
             let minXDist = t1HalfW + t2HalfW;
@@ -442,50 +442,54 @@ class Game {
 
         parent.context.fillStyle = this.background;
 
-        this.shape(parent.context, this.realX, this.realY, this.width, this.height);
+        this.shape(parent.context, this.realX, this.realY, this._data.width, this._data.height);
         if (this.destination) {
-          if ((this.destination[0] == '*' || this.destination[0] == this.x) && (this.destination[1] == '*' || this.destination[1] == this.y)) {
+          if ((this.destination[0] == '*' || this.destination[0] == this._data.x) && (this.destination[1] == '*' || this.destination[1] == this._data.y)) {
             this.destination = null;
             return;
           }
           let d = this.destination;
-          if (d[0] != '*' && this.x + this.vel.x / 1000 >= d[0] && this.vel.x > 0) {
+          if (d[0] != '*' && this._data.x + this.vel.x / 1000 >= d[0] && this.vel.x > 0) {
             this.vel.x = 0;
             this.x = d[0];
-          } else if (d[0] != '*' && this.x + this.vel.x / 1000 <= d[0] && this.vel.x < 0) {
+          } else if (d[0] != '*' && this._data.x + this.vel.x / 1000 <= d[0] && this.vel.x < 0) {
             this.vel.x = 0;
             this.x = d[0];
           }
-          if (d[1] != '*' && this.y + this.vel.y / 1000 >= d[1] && this.vel.y > 0) {
+          if (d[1] != '*' && this._data.y + this.vel.y / 1000 >= d[1] && this.vel.y > 0) {
             this.vel.y = 0;
             this.y = d[1];
-          } else if (d[1] != '*' && this.y + this.vel.y / 1000 <= d[1] && this.vel.y < 0) {
+          } else if (d[1] != '*' && this._data.y + this.vel.y / 1000 <= d[1] && this.vel.y < 0) {
             this.vel.y = 0;
             this.y = d[1];
           }
         }
       }
       teleport(x, y) {
-        this.x = x;
-        this.y = y;
-        this._data.x = val;
-        this._data.left = val - this.width / 2;
-        this._data.right = val + this.width / 2;
-        this._data.y = val;
-        this._data.top = val - this.height / 2;
-        this._data.bottom = val + this.height / 2;
-        //optimize by calculating everything here and only setting _data.x, y
+        this._data.x = x;
+        this._data.left = x - this._data.width / 2;
+        this._data.right = x + this._data.width / 2;
+        this._data.y = y;
+        this._data.top = y - this._data.height / 2;
+        this._data.bottom = y + this._data.height / 2;
       }
       moveX(amt) {
-        this.x += amt;
+        this._data.x += amt;
+        this._data.left += amt;
+        this._data.right += amt;
       }
       moveY(amt) {
-        this.y += amt;
+        this._data.y += amt;
+        this._data.top += amt;
+        this._data.bottom += amt;
       }
       move(x, y) {
-        this.x += x;
-        this.y += y;
-        //optimize by calculating everything here and only setting _data.x, y
+        this._data.x += amt;
+        this._data.left += amt;
+        this._data.right += amt;
+        this._data.y += amt;
+        this._data.top += amt;
+        this._data.bottom += amt;
       }
       to(x, y, speedX, speedY) {
         if (x == '*') speedX = 0;
@@ -495,9 +499,9 @@ class Game {
 
         this.destination = [x, y];
 
-        if (x > this.x) this.vel.x = speedX;
+        if (x > this._data.x) this.vel.x = speedX;
         else this.vel.x = -speedX;
-        if (y > this.y) this.vel.y = speedY;
+        if (y > this._data.y) this.vel.y = speedY;
         else this.vel.y = -speedY;
       }
     }
